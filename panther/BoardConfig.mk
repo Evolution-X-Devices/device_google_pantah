509f3e321b9a5bbc1c1868ce529f90db3fe2ad87
AnierinB <anierin@evolution-x.org>
2024-04-22 13:54:00 -0700

    pantah: Initialize for Evolution X 10.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    
    panthah: Fix status bar padding

--- panther/BoardConfig.mk
+++ panther/BoardConfig.mk
@@ -49,3 +49,4 @@ endif
 DEVICE_PATH := device/google/pantah
 VENDOR_PATH := vendor/google/panther
 include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigLineage.mk
+include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigEvolution.mk

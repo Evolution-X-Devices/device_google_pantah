818b8d788dcd2d7ba3b0c437930950e46aa15c2f
nhansp <nhan.094.vn@gmail.com>
2026-01-28 16:18:42 +0700

    pantah: Initialize for Evolution X 11.x
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

---
+++ cheetah/device-evolution.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: 2026 The Evolution X Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/$(DEVICE_CODENAME)/overlay-evolution
+
+TARGET_HAS_UDFPS += true
+
